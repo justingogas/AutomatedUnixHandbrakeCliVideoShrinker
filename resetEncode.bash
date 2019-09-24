78e1433cc31c6db047d093c909e87dc925087d38
Justin Gogas <justingogas@gmail.com>
2019-09-23 20:26:22 -0600

    Another bug fix that now requires the entire path to the location where the encode folders are located in order to work.

--- resetEncode.bash
+++ resetEncode.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Run this script on startup to reset the encoding state.  Parameter 1 is the directory that the encode script and folders are placed.
+# Run this script on startup to reset the encoding state.  Parameter 1 is the directory that the encode script and folders are placed.  For example, /home/<current user>/Videos/encode
 
 # Run on startup in Debian 10: https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux
 
-path="$1/Videos/encode"
-
-rm $path/working/*
-mv $path/queue/* $path/source
+rm $1/working/*
+mv $1/queue/* $1/source
